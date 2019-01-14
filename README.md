47f13e72bc4e88144df95be78140b7ca437d3669
Yarharm <yarharm@gmail.com>
2019-01-13 20:55:20 -0500

    Probable models: SVC or RandomForest.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kaggle-Competitions
 Set of competitions for ML and DL from https://www.kaggle.com/
+
+1) [Titanic](https://www.kaggle.com/c/titanic).
+2) [Blue Book for Bulldozers](https://www.kaggle.com/c/bluebook-for-bulldozers).
--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -1,24 +1,18 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline
 import pandas as pd
 import numpy as np
 import re
 
 
-df = pd.read_csv('train.csv', low_memory=False)
-
+train = pd.read_csv('train.csv', low_memory=False)
+y_train = train['Survived']
+df = pd.read_csv('data.csv', low_memory=False)
 # Encode Sex column (Nominal feature => OneHotEncoding)
 convert_sex = pd.get_dummies(df['Sex'], drop_first=True)
 df = df.join(convert_sex)
-
 # Embarked column
-df['Embarked'] = df['Embarked'].fillna('C')
-convert_embarked = pd.get_dummies(df['Embarked'], drop_first=True)
+df['Embarked'] = df['Embarked'].fillna('S')
+convert_embarked = pd.get_dummies(df['Embarked'])
 df = df.join(convert_embarked)
 
 # Title is the only relevant info in Name column
@@ -48,74 +42,66 @@ deck = {'A': 0, 'B': 1, 'C': 2,
         'D': 3, 'E': 4, 'F': 5,
         'G': 6, 'T': 7, 'U': 8}
 df['Cabin'] = df['Cabin'].map(deck)
-#print(df[['Cabin', 'Survived']].groupby(['Cabin']).mean())
 
-# Impute missing age values
-temp_df = df.copy()
-temp_df.dropna(inplace=True, subset=['Age'])
-mean_ = int(temp_df['Age'].mean())
-df.fillna(mean_, inplace=True)
-df['Age'] = df['Age'].astype('int64')
+# Impute missing Age values
+#temp_df = df.copy()
+#temp_df.dropna(inplace=True, subset=['Age'])
+#mean_ = int(temp_df['Age'].median())  # Median
+#df.fillna(mean_, inplace=True)
+#df['Age'] = df['Age'].astype('int64')
+
+index_NaN_age = list(df["Age"][df["Age"].isnull()].index)
+
+for i in index_NaN_age :
+    age_med = df["Age"].median()
+    age_pred = df["Age"][((df['SibSp'] == df.iloc[i]["SibSp"])
+                          & (df['Parch'] == df.iloc[i]["Parch"])
+                          & (df['Pclass'] == df.iloc[i]["Pclass"]))].median()
+    if not np.isnan(age_pred) :
+        df['Age'].iloc[i] = age_pred
+    else :
+        df['Age'].iloc[i] = age_med
 
 # Identify family sizes
-df['FamilySize'] = df['SibSp'] + df['Parch']
+df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+# Split family sizes
+df['Single'] = df['FamilySize'].map(lambda s: 1 if s == 1 else 0)
+df['SmallF'] = df['FamilySize'].map(lambda s: 1 if s == 2 else 0)
+df['MedF'] = df['FamilySize'].map(lambda s: 1 if 3 <= s <= 4 else 0)
+df['LargeF'] = df['FamilySize'].map(lambda s: 1 if s >= 5 else 0)
 
+# Map Fare
+def map_fare(price):
+    if price <= 8:
+        return 0.0
+    elif price > 8 and price <= 15:
+        return 1.0
+    elif price > 15 and price <= 31:
+        return 2.0
+    else:
+        return 3.0
+df['Fare'] = df['Fare'].apply(map_fare)
+# List of dropped columns
 drop_list = ['Sex', 'Embarked',
              'SibSp', 'Parch',
              'Ticket', 'Name']
 df.drop(drop_list, inplace=True, axis=1)
 df = df.apply(pd.to_numeric, errors='coerce', axis=1)
 # TRAINING
-X_train = df.drop('Survived',axis=1)
-y_train = df['Survived']
-
-# ::=> SIMPLE RANDOM FOREST
-#m = RandomForestClassifier(n_jobs=-1, n_estimators=10)
-#m.fit(X_train,y_train)
-#print(m.score(X_train,y_train))
-
-# ::=> PIPELINE WITH RANDOM FOREST AND K-FOLD VALIDATION
-#pipe = make_pipeline(StandardScaler(),
-#                     RandomForestClassifier(n_estimators=100,
-#                                            random_state=1))
-#pipe.fit(X_train, y_train)
-#y_pred = pipe.predict(X_test)  # TEST CASE HERE
-#print("Accuracy: %.3f" % pipe.score(X_train, y_train))
-# K-fold cross-validation
-#scores = cross_val_score(estimator=pipe,
-#                         X=X_train,
-#                         y=y_train,
-#                         cv=10,
-#                         n_jobs=-1)
-#print('Accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
-
-# SVC
-pipe = make_pipeline(StandardScaler(),
-                     SVC(random_state=1))
-param_range = [0.0001, 0.001, 0.01, 0.1,
-               1.0, 10.0, 100.0, 1000.0]
-
-param_grid = [{'svc__C': param_range,
-               'svc__kernel': ['linear']},
-              {'svc__C': param_range,
-               'svc__kernel': ['rbf'],
-               'svc__gamma': param_range}]
-
-#gs = GridSearchCV(estimator=pipe,
- #                 param_grid=param_grid,
- #                 scoring='accuracy',
- #                 cv = 10,
- #                 n_jobs=-1)
-#gs = gs.fit(X_train, y_train)
-#print("Score %.3f" % gs.best_score_)
-#print(gs.best_params_)
-
-# SCV WITH NESTED CROSS-VALIDATION
-gs = GridSearchCV(estimator=pipe,
-                  param_grid=param_grid,
-                  scoring='accuracy',
-                  cv=2)
-
-scores = cross_val_score(gs, X_train, y_train,
-                         scoring='accuracy', cv=5)
-print('Accuracy %.3f' % np.mean(scores))
+X_train = df.iloc[:891, :]
+X_test = df.iloc[891:, :]
+m = RandomForestClassifier(n_estimators=60,
+                           n_jobs=-1,
+                           random_state=1,
+                           criterion='gini',
+                           max_depth=8,
+                           max_features='auto')
+m = m.fit(X_train,y_train)
+print('Score on training set: %.3f' % m.score(X_train, y_train))
+predicted = m.predict(X_test)
+result = pd.DataFrame({'PassengerId': X_test['PassengerId'],
+                       'Survived': predicted})
+result['PassengerId'] = result['PassengerId'].astype('Int32')
+result['Survived'] = result['Survived'].astype('Int32')
+print(result.dtypes)
+result.to_csv('submission.csv', index=False)
