8853011471d11af0e22745b76d1a2b3c566a2780
Yaroslav Harmatyuk <42894823+Yarharm@users.noreply.github.com>
2019-02-05 21:01:49 -0500

    Basic LogisticRegr test. Start RNN build

--- Bag of Words/model_Keras.py
+++ Bag of Words/model_Keras.py
@@ -1,6 +1,19 @@
 import pandas as pd
-import string
+import numpy as np
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+import re
+# RNN imports
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding
+
 
 # 25000 x 3 {id, sentiment, review}
 df = pd.read_csv('labeledTrainData.tsv', sep='\t')
@@ -8,27 +21,55 @@ review_ID = df['id']
 df = df.drop(['id'], axis=1)
 #print(df.shape)
 
-# Preprocess data
+## Preprocessing
 stop_words = set(stopwords.words('english'))
 
+# Remove <br /> occurabces
+df['review'] = df['review'].map(lambda row: re.sub(r'<br />', '', row))
+
 # Splitting into Tokens
-#print(df['review'].head(1))
-df['review'] = df['review'].map(lambda row: row.split())
-#print(df['review'].head(1))
+tokenizer = RegexpTokenizer(r'\w+')  # Pick alphanumeric characters and drop the rest
+df['review'] = df['review'].map(lambda row: tokenizer.tokenize(row))
 
-# Remove punctuation
-tr_table = str.maketrans('', '', string.punctuation)
-df['review'] = df['review'].map(lambda row: [word.translate(tr_table) for word in row])
-#print(df['review'].head(1))
-
-# Remove tokens that are not alphabetical
+# Remove tokens that are not alphabetical (Numbers)
 df['review'] = df['review'].map(lambda row: [word for word in row if word.isalpha()])
-#print(df['review'].head(1))
 
 # Remove stop words
 df['review'] = df['review'].map(lambda row: [word for word in row if not word in stop_words])
-#print(df['review'].head(1))
 
-# Remove very short tokens
-df['review'] = df['review'].map(lambda row: [word for word in row if len(word) > 1])
+# Reduce words to its root
+stemmer = PorterStemmer()
+df['review'] = df['review'].map(lambda row: ' '.join([stemmer.stem(word) for word in row]))
 
+# Input and target data
+X_train, X_test, y_train, y_test = train_test_split(df['review'], df['sentiment'],
+                                                    test_size=0.2, random_state=1)
+
+## Performance analysis of a simple Logistic Regression
+# Transform text to feature vectors and feed it to the estimator
+lg_pipe = Pipeline([
+    ('tfidf', TfidfVectorizer(max_df=0.9)),
+    ('lg', LogisticRegression(n_jobs=-1))])
+#lg_pipe.fit(X_train, y_train)
+#print('LogReg accuracy: %.4f' % lg_pipe.score(X_test, y_test))
+
+# RNN Model
+vocab_size = 20000  # Max number of words to keep
+
+# Remove punctuation, tokenize and vectorize inputs
+tokenz = Tokenizer(num_words=vocab_size)
+tokenz.fit_on_texts(df['review'])
+sequences = tokenz.texts_to_sequences(df['review'])
+
+# Pad sequences which are less than 250(Average review length)
+sequences = pad_sequences(sequences, maxlen=250)
+print(sequences.shape)
+
+### Network layers: Embedded, LSTM, *Most likely*MaxPool, Dense, Dropout(Like CNN), Dense
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size, output_dim=128, input_length=250, name='embd'))
+#model.add(Bidirectional(LSTM(???????????????????????))
+# !!!!!!!!MaxPool or AverPool!!!!!!!!!!
+#model.add(Dense(????????????))
+#model.add(Dropout(?????????))
+#model.add(Dense(????????))
