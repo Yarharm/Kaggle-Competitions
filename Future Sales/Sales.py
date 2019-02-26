10baf7b62b4ba15ea6184653f74b67ee9cfa40c4
Yaroslav Harmatyuk <42894823+Yarharm@users.noreply.github.com>
2019-02-25 19:49:47 -0500

    LightGBM solution analysis

--- Future Sales/Sales.py
+++ Future Sales/Sales.py
@@ -5,160 +5,70 @@ import seaborn as sns
 from scipy.stats import norm
 import re
 import datetime
+from lightgbm import LGBMRegressor
 import warnings
 
 # set warnings
 warnings.filterwarnings('ignore')
 
 # Get data
-df = pd.read_csv('sales_train_v2.csv', low_memory=False)
+df = pd.read_csv('sales_train_v2.csv', low_memory=False, parse_dates=['date'],
+                 infer_datetime_format=True, dayfirst=True)  # Object to Date
 test_set = pd.read_csv('test.csv', low_memory=False)
 shops = pd.read_csv('shops.csv', low_memory=False)
 item_cat = pd.read_csv('item_categories.csv', low_memory=False)
 items = pd.read_csv('items.csv', low_memory=False)
 
-# Duplicated rows
-#print(df[df.duplicated(keep=First)])
-#df.drop_duplicates()
-
-## MEMORY SAVING MECHANISM(DOWNCASTING)
-def downcast_dtypes(df):
-    float_cols = [c for c in df if df[c].dtype == "float64"]
-    int_cols = [c for c in df if df[c].dtype in ["int64", "int32"]]
-    df[float_cols] = df[float_cols].astype(np.float32)
-    df[int_cols] = df[int_cols].astype(np.int16)
-    return df
-
-
+# Monthly sales
+df = df.groupby([df.date.apply(lambda x: x.strftime('%Y-%m')), 'item_id', 'shop_id']).sum().reset_index()
+df = df[['date', 'item_id', 'shop_id', 'item_cnt_day']]
 # Create pivot table
-sales_by_item_id = df.pivot_table(index=['item_id'], values=['item_cnt_day'],
-                                  columns='date_block_num', aggfunc=np.sum, fill_value=0).reset_index()
-sales_by_item_id.columns = sales_by_item_id.columns.droplevel().map(str)
-sales_by_item_id = sales_by_item_id.reset_index(drop=True).rename_axis(None, axis=1)
-sales_by_item_id.columns.values[0] = 'item_id'
-#print(sales_by_item_id)
+df = df.pivot_table(values='item_cnt_day', index=['item_id', 'shop_id'],
+                    columns='date', fill_value=0).reset_index()
 
+# Merge train set with pivot table
+df = pd.merge(test_set, df, on=['item_id','shop_id'], how='left')
+df = df.fillna(0)
 
-# Products with no sales for the last 6 months
-outdated_items = sales_by_item_id[sales_by_item_id.loc[:, '27':].sum(axis=1) == 0]
-print(outdated_items)
+# Drop categorical data
+df.drop(['item_id', 'shop_id'], axis=1, inplace=True)
 
-## Format Date column
-# Convert from string(object) to the datetime object(datetime64)
-#df['date'] = df['date'].map(lambda x: datetime.datetime.strptime(x, '%d.%m.%Y'))
-#df['day'] = df['date'].apply(lambda x: x.day)
-#df['month'] = df['date'].apply(lambda x: x.month)
-#df['year'] = df['date'].apply(lambda x: x.year)
-#df['weekend'] = df['date'].apply(lambda x: x.weekday())  # 0 = Monday, 6 = Sunday
+# Train/Test taking target final available month '2015-10'
+target = '2015-10'
+y_train = df[target]
+X_train = df.drop(labels=[target], axis=1)
+print('X_train: ', X_train.shape)
 
-#sales = df.groupby(['date_block_num', 'shop_id', 'item_id']).agg({'date': ['min', 'max'],
-#                                                                  'item_price': 'mean',
-#                                                                  'item_cnt_day': 'sum'})
+# Keras adjustment
+X_train = X_train.as_matrix()
+X_train = X_train.reshape((214200, 33, 1))
 
-# Left join item_category_id to the main DataFrame
-#df = df.join(items['item_category_id'])
-#print(df.columns)
+y_train = y_train.as_matrix()
+y_train = y_train.reshape(214200, 1)
 
+# Drop first month for LSTM memorization
+X_test = df.drop(labels=['2013-01'], axis=1)
+X_test = X_test.as_matrix()
+X_test = X_test.reshape((214200, 33, 1))
+print('X_train: ', X_train.shape)
+print('X_test: ', X_test.shape)
+print('y_train: ', y_train.shape)
 
+# Model
+m = LGBMRegressor(n_estimators=200,
+                  learning_rate=0.03,
+                  num_leaves=32,
+                  colsample_bytree=0.9497036,
+                  subsample=0.8715623,
+                  max_depth=8,
+                  reg_alpha=0.04,
+                  reg_lambda=0.073,
+                  min_split_gain=0.0222415,
+                  min_child_weight=40)
 
+m.fit(X_train, y_train)
+y_pred = m.predict(X_test).clip(0., 20.)
 
-
-
-# Add item count per month
-#mapping = df.groupby('date_block_num').item_cnt_day.sum()
-#df['item_cnt_month'] = df['date_block_num'].apply(lambda x: mapping[x])
-#print(df.tail(2).transpose())
-
-# Total sales per month
-m_sales = df.groupby('date_block_num').item_cnt_day.sum()
-def montly_sales(sales):
-    plt.figure(figsize=(12, 8))
-    plt.ylabel('Sales')
-    plt.xlabel('Time')
-    plt.title('Sales per month')
-    plt.plot(sales)
-    plt.show()
-    ## Conclusion: Observed seasonality
-
-# Explore trend, seasonality and residuals
-def stats(sales):
-    residual_1 = sm.tsa.seasonal_decompose(sales.values, freq=12, model='multiplicative')
-    residual_1.plot()
-    residual_2 = sm.tsa.seasonal_decompose(sales.values, freq=12, model="additive")
-    residual_2.plot()
-    plt.show()
-
-# Single series analysis
-def get_tables():
-    print(df.head(2).transpose())
-    print('\nShops')
-    print(shops.head(2).transpose())
-    print('\nItem Categories')
-    print(item_cat.head(2).transpose())
-    print('\nItems')
-    print(items.head(2).transpose())
-def get_df():
-    print(df.head(2).transpose())
-#print(test_set.head(3).transpose())
-#print(df.dtypes)
-#print(df.describe())
-#print(df.head(2).transpose())
-#get_tables()
-
-# Explore Target (CONTAINS OUTLIER)
-target = df['item_cnt_day']
-def target_pot():
-    fig, ax = plt.subplots(figsize=(16, 5))
-    sns.distplot(target, ax=ax)
-
-    # BoxPlot
-    fig, ax = plt.subplots(figsize=(12, 3))
-    sns.boxplot(x=target, data=df)
-
-    plt.show()
-## Combine categories in groups
-#print(item_cat['item_category_name'].unique())
-
-# Substitute category name by the first word only
-item_cat['item_category_name'] = item_cat['item_category_name'].map(lambda x: re.match('^([^\s]+)', x)[0])
-
-# Mapping item_id => item_category_id
-item_id_to_item_cat = {}
-for index, row in items.iterrows():
-    item_id_to_item_cat.update({row['item_id']: row['item_category_id']})
-
-# Mapping item_category_id => category_name
-item_cat_to_cat_name = {}
-for index, row in item_cat.iterrows():
-    item_cat_to_cat_name.update({row['item_category_id']: row['item_category_name']})
-
-# Map item ids
-df['item_name'] = df['item_id'].map(lambda x: item_cat_to_cat_name[item_id_to_item_cat[x]])
-
-## UTILIZE SHOP NAMES
-
-# Separate by shop location (Moskva, Omsk) and shop type (TRK, TC)
-# Mapping shop_id => shop_name
-shop_id_to_shop_name = {}
-for index, row in shops.iterrows():
-    shop_id_to_shop_name.update({row['shop_id']: row['shop_name']})
-
-df['shop_location'] = df['shop_id'].map(lambda x: shop_id_to_shop_name[x].split()[0])
-df['shop_type'] = df['shop_id'].map(lambda x: shop_id_to_shop_name[x].split()[1])
-
-
-## Explore item_price (CONTAINT OUTLIER => BOXPLOT)
-def price_plot():
-    sns.set(style='whitegrid')
-    sns.boxplot(x=df['item_price'])
-
-    fig, ax = plt.subplots(figsize=(16, 8))
-    ax.scatter(df['item_price'], df['item_cnt_day'])
-    ax.set_xlabel('Price for the item distribution')
-
-    plt.show()
-
-
-# APPENDING PARTIAL NOMINAL DATA TO THE DF
-#dummy = pd.get_dummies(df['item_name'])
-#df = pd.concat([df, dummy], axis=1)
+# Submission
+preds = pd.DataFrame(y_pred, column=['item_cnt_month'])
+preds.to_csv('sub.csv', index_label='ID')
