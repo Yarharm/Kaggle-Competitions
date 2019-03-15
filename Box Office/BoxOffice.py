e5e403daf37f869138492170354a376ba2bf68b5
Yaroslav Harmatyuk <42894823+Yarharm@users.noreply.github.com>
2019-03-14 22:03:30 -0400

    Feature adjustment and 3-d plot analysis

--- Box Office/BoxOffice.py
+++ Box Office/BoxOffice.py
@@ -142,6 +142,45 @@ y_train = np.log1p(y_train)
 # sns.distplot(y_train)
 # plt.show()
 
+# Extra data from the Kernel
+train.loc[train['id'] == 16,'revenue'] = 192864          # Skinning
+train.loc[train['id'] == 90,'budget'] = 30000000         # Sommersby          
+train.loc[train['id'] == 118,'budget'] = 60000000        # Wild Hogs
+train.loc[train['id'] == 149,'budget'] = 18000000        # Beethoven
+train.loc[train['id'] == 313,'revenue'] = 12000000       # The Cookout 
+train.loc[train['id'] == 451,'revenue'] = 12000000       # Chasing Liberty
+train.loc[train['id'] == 464,'budget'] = 20000000        # Parenthood
+train.loc[train['id'] == 470,'budget'] = 13000000        # The Karate Kid, Part II
+train.loc[train['id'] == 513,'budget'] = 930000          # From Prada to Nada
+train.loc[train['id'] == 797,'budget'] = 8000000         # Welcome to Dongmakgol
+train.loc[train['id'] == 819,'budget'] = 90000000        # Alvin and the Chipmunks: The Road Chip
+train.loc[train['id'] == 850,'budget'] = 90000000        # Modern Times
+train.loc[train['id'] == 1112,'budget'] = 7500000        # An Officer and a Gentleman
+train.loc[train['id'] == 1131,'budget'] = 4300000        # Smokey and the Bandit   
+train.loc[train['id'] == 1359,'budget'] = 10000000       # Stir Crazy 
+train.loc[train['id'] == 1542,'budget'] = 1              # All at Once
+train.loc[train['id'] == 1542,'budget'] = 15800000       # Crocodile Dundee II
+train.loc[train['id'] == 1571,'budget'] = 4000000        # Lady and the Tramp
+train.loc[train['id'] == 1714,'budget'] = 46000000       # The Recruit
+train.loc[train['id'] == 1721,'budget'] = 17500000       # Cocoon
+train.loc[train['id'] == 1865,'revenue'] = 25000000      # Scooby-Doo 2: Monsters Unleashed
+train.loc[train['id'] == 2268,'budget'] = 17500000       # Madea Goes to Jail budget
+train.loc[train['id'] == 2491,'revenue'] = 6800000       # Never Talk to Strangers
+train.loc[train['id'] == 2602,'budget'] = 31000000       # Mr. Holland's Opus
+train.loc[train['id'] == 2612,'budget'] = 15000000       # Field of Dreams
+train.loc[train['id'] == 2696,'budget'] = 10000000       # Nurse 3-D
+train.loc[train['id'] == 2801,'budget'] = 10000000       # Fracture
+test.loc[test['id'] == 3889,'budget'] = 15000000       # Colossal
+test.loc[test['id'] == 6733,'budget'] = 5000000        # The Big Sick
+test.loc[test['id'] == 3197,'budget'] = 8000000        # High-Rise
+test.loc[test['id'] == 6683,'budget'] = 50000000       # The Pink Panther 2
+test.loc[test['id'] == 5704,'budget'] = 4300000        # French Connection II
+test.loc[test['id'] == 6109,'budget'] = 281756         # Dogtooth
+test.loc[test['id'] == 7242,'budget'] = 10000000       # Addams Family Values
+test.loc[test['id'] == 7021,'budget'] = 17540562       #  Two Is a Family
+test.loc[test['id'] == 5591,'budget'] = 4000000        # The Orphanage
+test.loc[test['id'] == 4282,'budget'] = 20000000
+
 
 ## EDA
 # Pair plot for Discrete variables
@@ -205,6 +244,22 @@ df.drop(labels, axis=1, inplace=True)  # Drop encoded categorical data
 df = pd.concat([df, labels_encoded], axis=1)  # Append encoded labels
 
 
+# 3d Plot
+release_year_mean_data=train.groupby(['release_year'])['budget','popularity','revenue'].mean()
+release_year_mean_data.head()
+
+fig = plt.figure(figsize=(13,13))
+ax = plt.subplot(111,projection = '3d')
+
+# Data for three-dimensional scattered points
+zdata =train.popularity
+xdata =train.budget
+ydata = train.revenue
+ax.scatter3D(xdata, ydata, zdata, c=zdata, s = 200)
+ax.set_xlabel('Budget of the Movie',fontsize=17)
+ax.set_ylabel('Revenue of the Movie',fontsize=17)
+ax.set_zlabel('Popularity of the Movie',fontsize=17)
+plt.show()
 
 ## Modeling
 print(df.shape)
